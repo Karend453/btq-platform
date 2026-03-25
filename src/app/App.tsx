cc9fbc18bda15e3cb14810654e9536b289b4f045
Karen DeFelice <admin@brokerteq.com>
2026-03-24 21:36:32 -0400

    test routes.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,5 +3,9 @@ import { router } from "./routes";
 import { AuthProvider } from "./contexts/AuthContext";
 
 export default function App() {
-  return <div>APP TEMP TEST</div>;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
